c4afda21f3885f11d645c11a1acdad6a38f3e311
Ez_Song <419097924@qq.com>
2022-06-14 17:18:24 +0800

    单元测试

--- bob/hello/hello.go
+++ bob/hello/hello.go
@@ -1,6 +1,8 @@
 package hello
 
+const englishHelloPrefix = "Hello, "
+
 func Hello(name string) string {
 
-	return "Hello, " + name
+	return englishHelloPrefix + name
 }
--- bob/hello/hellop_test.go
+++ bob/hello/hellop_test.go
@@ -4,12 +4,31 @@ import "testing"
 
 func TestHello(t *testing.T) {
 
-	got := Hello("chris")
-
-	want := "Hello, chris1"
-
-	if got != want {
-
-		t.Errorf("got %v, want %v", got, want)
+	assertCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got '%q' want '%q'", got, want)
+		}
 	}
+
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("chris")
+
+		want := "Hello, chris1"
+
+		if got != want {
+
+			t.Errorf("got %v, want %v", got, want)
+		}
+	})
+
+	t.Run("say hello world when an empty string is supplied", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, World"
+
+		if got != want {
+			t.Errorf("got '%q' want '%q'", got, want)
+		}
+	})
+
 }
