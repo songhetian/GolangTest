ca71af9e13decaecf7c67368883e34ae02271e3a
Ez_Song <419097924@qq.com>
2022-06-24 18:15:58 +0800

    切片求和和测试

--- test01/array_silce/sum.go
+++ test01/array_silce/sum.go
@@ -1,11 +1,41 @@
 package array_silce
 
 //数组计算总和
-func Sum(numbers [5]int) (sum int) {
+func Sum(numbers []int) (sum int) {
 	sum = 0
 
-	for i := 0; i < len(numbers); i++ {
-		sum += numbers[i]
+	// for i := 0; i < len(numbers); i++ {
+	// 	sum += numbers[i]
+	// }
+
+	for _, number := range numbers {
+
+		sum += number
+	}
+
+	return sum
+}
+
+func SumAll(numbers ...[]int) (sum []int) {
+
+	for _, number := range numbers {
+		sum = append(sum, Sum(number))
+	}
+	return sum
+}
+
+func SumAllTails(numbersToSum ...[]int) (sum []int) {
+
+	for _, numbers := range numbersToSum {
+
+		if len(numbers) == 0 {
+
+			sum = append(sum, 0)
+		} else {
+			tail := numbers[1:]
+
+			sum = append(sum, Sum(tail))
+		}
 	}
 
 	return sum
