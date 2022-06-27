640b21c7ae21974a616fe15d77ab4a6ae34e4beb
Ez_Song <419097924@qq.com>
2022-06-27 18:25:44 +0800

    测试周长v1

--- test01/structs/v1/shapes.go
+++ test01/structs/v1/shapes.go
@@ -1,1 +1,8 @@
 package v1
+
+func Perimeter(height, width float64) (p float64) {
+
+	p = (height + width) * 2
+
+	return
+}
--- test01/structs/v1/shapes_test.go
+++ test01/structs/v1/shapes_test.go
@@ -1,1 +1,21 @@
 package v1
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestPerimeter(t *testing.T) {
+	assert := func(t *testing.T, got, want float64) {
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("value: got %.2f, want %.2f", got, want)
+		}
+	}
+
+	t.Run("strurt1", func(t *testing.T) {
+		got := Perimeter(10.0, 10.0)
+		want := 40.0
+		assert(t, got, want)
+	})
+}
